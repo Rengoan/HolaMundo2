95daa845266681a97fad611087887dd7b91dddc9
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-09-22 11:52:24 +0200

    Update HolaMundo2.java

--- HolaMundo2/src/holamundo2/HolaMundo2.java
+++ HolaMundo2/src/holamundo2/HolaMundo2.java
@@ -14,23 +14,21 @@ public class HolaMundo2 {
     /**
      * @param args the command line arguments
      */
-    
 //    Ejemplo basico
-    
 //    psvm para escribir public static void main(String[] args)
     public static void main(String[] args) {
         // TODO code application logic here
         
-       String miVar = "Hola profesor";
+        String miVar = "Hola profesor";
 //       soutv atajo para System.out.println("miVar = " + miVar);
         System.out.println("miVar = " + miVar);
 //        sout atajo System.out.println("Hola alumno");
         System.out.println("Hola alumno");
         
-       int piso= 2;
-       
+        var piso = 2;
+        
         System.out.println("piso = " + piso);
-    
+        
     }
     
 }
