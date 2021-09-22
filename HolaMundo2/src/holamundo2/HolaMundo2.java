a6ef8949d24a61f791484c24d589ae57d55d234a
Rengoan <91058472+Rengoan@users.noreply.github.com>
2021-09-22 11:47:44 +0200

    Prueba atajos

---
+++ HolaMundo2/src/holamundo2/HolaMundo2.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo2;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class HolaMundo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+//    Ejemplo basico
+    
+//    psvm para escribir public static void main(String[] args)
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       String miVar = "Hola profesor";
+//       soutv atajo para System.out.println("miVar = " + miVar);
+        System.out.println("miVar = " + miVar);
+//        sout atajo System.out.println("Hola alumno");
+        System.out.println("Hola alumno");
+        
+       int piso= 2;
+       
+        System.out.println("piso = " + piso);
+    
+    }
+    
+}
